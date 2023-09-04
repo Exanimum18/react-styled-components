7b343c872f57dc2262fe9c20734e16fea0aba2dd
Exanimum18 <santiago.baron.18@gmail.com>
2023-09-04 11:11:00 -0500

    Usando los styled components en los componentes de Account, Container, Header y todo el archivo de stilos.css quedo comentado

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
-
+import { styled } from "styled-components";
+import { Icono, Box, Btn, Saldo, Detalle } from "../UI";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
+
+//Herencia
+const IconMargin = styled(Icono)`
+  margin-top: 2px;
+`
 
 const Account = () => {
   const [toggleState, untoggle] = useState(true);
@@ -12,29 +18,39 @@ const Account = () => {
   };
 
   return (
-    <div className="box">
+    // <div className="box">
+    <Box>
       <h2>Cuenta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
         <span>
-          <img className="imagen-icono" src={dinero} alt="Icono de saldo" />
+          <Icono src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalle">$</span> 8,621.50
-          </div>
+          <Saldo>
+          {/* <div className="saldo"> */}
+            <Detalle>
+              $
+            </Detalle>
+            8,621.50
+            {/* <span className="detalle">$</span> 8,621.50 */}
+          {/* </div> */}
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagen-icono"
+      {/* <button className="btn" onClick={toggleHandler}> */}
+      <Btn onClick={toggleHandler}>
+        <IconMargin
+          // style={{ marginTop: "2px" }}
+          // className="imagen-icono"
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
-      </button>
-    </div>
+      </Btn>
+      {/* </button> */}
+    </Box>
+    // </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./assets/estilos.css";
+// import "./assets/estilos.css";
 
 ReactDOM.render(
-    <App />, 
+    <App />,
     document.getElementById("root")
 );
